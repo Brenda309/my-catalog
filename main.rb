7a48a798c632777cf219cc51b459a5f9c877a852
Brenda309 <brewihogora23@gmail.com>
2022-09-13 00:58:20 +0200

    Add the option list

---
+++ main.rb
@@ -0,0 +1,21 @@
+require './lib/item'
+
+
+def list_of_options
+  puts 'Please choose an option by entering a number:'
+  puts '============================================='
+  puts ''
+  puts '1 - List all books'
+  puts '2 - List all music albums'
+  puts '3 - List all movies'
+  puts '4 - List all genres'
+  puts '5 - List all label'
+  puts '6 - List all authors'
+  puts '7 - List all sources'
+  puts 'A - Add a book'
+  puts 'B - Add a music album'
+  puts 'C - Add a movie'
+  puts '0 - exit'
+end
+
+list_of_options
