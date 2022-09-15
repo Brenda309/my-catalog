eac4e4a8abb6c9f149dbba0e53161c86074f2f2d
Brenda309 <brewihogora23@gmail.com>
2022-09-15 22:14:46 +0200

    Fix linters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/book'
 describe Book do
   context 'Testing the class books catalog' do
     before :each do
-      @book = Book.new( "science","Brenda","na","unknown","09-09-2001","phouse","good") 
+      @book = Book.new('science', 'Brenda', 'na', 'unknown', '09-09-2001', 'phouse', 'good')
     end
 
     it '@book should be an instance of the class Book' do
@@ -22,7 +22,7 @@ describe Book do
       expect(@book).to respond_to(:can_be_archived?)
     end
 
-     it 'the date should be' do
+    it 'the date should be' do
       expect(@book.publish_date).to eq('09-09-2001')
     end
   end
