628aeea75b02d12f66b3fac2bd8e6b6361f7dbf8
Brenda309 <brewihogora23@gmail.com>
2022-09-15 23:08:37 +0200

    Update the label_spec.rb file

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -6,19 +6,19 @@ describe Label do
       @label = Label.new('programming', 'blue')
     end
 
-    it '@label should be an instance of the class Author' do
+    it '@label should be an instance of the class label' do
       expect(@label).to be_instance_of Label
     end
 
-    it "The author1's first name should be John" do
+    it "The author1's first name should be programming do"
       expect(@label.title).to eq('programming')
     end
 
-    it "The author1's last name should be Doe" do
+    it 'the label color should be blue' do
       expect(@label.color).to eq('blue')
     end
 
-    it 'The method add_item should be available for the instance @author1' do
+    it 'The method add_item should be available for the instance label' do
       expect(@label).to respond_to(:add_item)
     end
   end
