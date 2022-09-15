64da1b6d639747c708f0a5e01b55da33a807fb68
Hans Zizold <hzizold@hotmail.com>
2022-09-15 18:07:14 -0400

    Final adjusts
    Final corrections executed
    Tests are OK!

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,6 @@
 class Item
   attr_reader :genre, :author, :label
-  attr_accessor :source, :publish_date
+  attr_accessor :source, :publish_date, :archived
 
   def initialize(genre, author, source, label, publish_date)
     @id = rand(1..1000)
@@ -17,10 +17,6 @@ class Item
     genre.add_item(self) unless genre.items.include?(self)
   end
 
-  def can_be_archived?
-    return true if Time.now.year - @publish_date[0..3].to_i > 10
-  end
-
   def author=(author)
     @author = author
     author.add_item(self) unless author.items.include?(self)
@@ -31,9 +27,16 @@ class Item
     label.add_item(self) unless label.items.include?(self)
   end
 
+  def can_be_archived?
+    return true if Time.now.year - @publish_date[0..3].to_i > 10
+
+    false
+  end
+
   def move_to_archive
     @archived = true if can_be_archived? == true
 
-    false
+    @archived
+
   end
 end
--- lib/label.rb
+++ lib/label.rb
@@ -2,10 +2,10 @@ class Label
   attr_reader :id, :items
   attr_accessor :color, :title
 
-  def initialize(title, color, id = Random.rand(1..100))
-    @id = id
-    @color = color
+  def initialize(title, color)
+    @id = rand(1..1000)
     @title = title
+    @color = color
     @items = []
   end
 
